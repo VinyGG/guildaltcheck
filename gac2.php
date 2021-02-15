2122e1095b817bea01dc1a44de4202ac792fdb6e
viny <ggviny@gmail.com>
2021-02-15 16:57:18 -0300

    index

--- gac2.php
+++ gac2.php
@@ -1,33 +1,53 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <?php
-$token = "USIfJFShESg0XRTCHbavKmuMEcHUPd4PhY";
+echo "viny";
+
 $region = "us";
 $realm = "azralon";
 $guild = "saltlords";
-$iterJson = "http://localhost/guildaltcheck/saltlords.json";
-$urlName = "https://".$region.".api.blizzard.com/data/wow/guild/".$realm."/".$guild."/roster?namespace=profile-us&locale=en_US&access_token=".$token;
-$urlAchiev;
-$members = json_decode(file_get_contents($urlName), true);
-$names = $members['members'];
+$key = "USIfJFShESg0XRTCHbavKmuMEcHUPd4PhY";
+//$url = "https://us.api.blizzard.com/wow/character/azralon/Domy?locale=en_US&access_token=USIfJFShESg0XRTCHbavKmuMEcHUPd4PhY";
+
+//Guild Members
+$url = "http://".$region.".api.blizzard.com/wow/guild/".$realm."/".$guild."?fields=members&locale=en_US&access_token=".$key;
+$ch = curl_init($url);
+print_r($ch);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$members = json_decode(curl_exec($ch));
+$members = $members->members;
+print_r($members);
 exit;
-foreach ( $names as $name ) { 
-    echo $name['character']['name']."<br>" ;
+$json = file_get_contents($url);
+$members = json_decode($json, true);
+print_r($members);
+exit;
+//GetAchievements
+function getAchiev($charName) {
+    global $region;
+    global $realm;
+    global $guild;
+    global $key;
+    $url2 = "https://".$region.".api.blizzard.com/wow/character/".$realm."/".$charName."?locale=en_US&access_token=".$key;
+    $ch2 = curl_init($url2);
+    curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+    $char = json_decode(curl_exec($ch2));
+    print_r($char);
+    return $char->achievementPoints;
+}
+
+foreach ($members as $member) {
+    $charName = $member->character->name;
+    echo $member->character->name . "(" . GetAchiev($member->character->name) . ")" . " | ";
 }
 exit;
+foreach ($members as $member) {
+    $charName = $member->character->name;
+    echo $charName . "(" . GetAchiev($charName) . ")" . " | ";
+    foreach ($members as $memberNext) {
+        if (GetAchiev($member->character->name) == GetAchiev($memberNext->character->name)) {
+            echo $memberNext->character->name . "(" . GetAchiev($memberNext->character->name) . ") ";
+        }
+    }
+    echo "\n";
+}
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        ?>
-    </body>
-</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+$token = "USIfJFShESg0XRTCHbavKmuMEcHUPd4PhY";
+$region = "us";
+$realm = "azralon";
+$guild = "saltlords";
+$interJson = "http://localhost/guildaltcheck/saltlords.json";
+$urlName = "https://".$region.".api.blizzard.com/data/wow/guild/".$realm."/".$guild."/roster?namespace=profile-us&locale=en_US&access_token=".$token;
+echo "urlName";
+exit;
+$urlAchiev;
+$members = json_decode(file_get_contents($urlName), true);
+exit;
+$names = $members['members'];
+exit;
+foreach ( $names as $name ) {
+    echo $name['character']['name']."<br>" ;
+}
+exit;
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+        ?>
+    </body>
+</html>
