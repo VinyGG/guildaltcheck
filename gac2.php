237ee3d732d5e98675f5c6950749358fb61f84e6
viny <ggviny@gmail.com>
2021-02-15 15:47:27 -0300

    new calc2

---
+++ gac2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+$token = "USIfJFShESg0XRTCHbavKmuMEcHUPd4PhY";
+$region = "us";
+$realm = "azralon";
+$guild = "saltlords";
+$iterJson = "http://localhost/guildaltcheck/saltlords.json";
+$urlName = "https://".$region.".api.blizzard.com/data/wow/guild/".$realm."/".$guild."/roster?namespace=profile-us&locale=en_US&access_token=".$token;
+$urlAchiev;
+$members = json_decode(file_get_contents($urlName), true);
+$names = $members['members'];
+exit;
+foreach ( $names as $name ) { 
+    echo $name['character']['name']."<br>" ;
+}
+exit;
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
